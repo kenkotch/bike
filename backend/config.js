ac6eb55b5542eff8c803c7ba04f95f352f642769
Ken Kotch <hello@ken.uno>
2017-11-17 19:56:45 -0700

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '355116831617012',
+  clientSecret: 'dcc6fe2b9766a604c08d8b1f3f03ff7a',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '212800111227-5alhcstm2hitugdm3t5qmv6iesk14fi4.apps.googleusercontent.com',
+  clientSecret: '9pNmncupVQA26SA1ncpWPu3K',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
