3f6229b8947484828829c46d6d3fa76777d581ce
Ken Kotch <hello@ken.uno>
2017-11-18 21:10:18 -0700

    update app.js - remove junk code

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ export default class App extends Component {
             <View style={styles.containter}>
               <Header />
             </View>
-          : // Show Please log in message if not
+          : // Show log in message if not
             <View>
               <Login
                 loginWithGoogle={ this.loginWithGoogle.bind(this) }
@@ -84,48 +84,7 @@ export default class App extends Component {
             </View>
         }
       </View>
-    );
+    )
 
   }
 }
-//
-// const iconStyles = {
-//   borderRadius: 10,
-//   iconStyle: { paddingVertical: 5 },
-// };
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#FFF',
-//   },
-//   content: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: 20,
-//   },
-//   avatarImage: {
-//     borderRadius: 50,
-//     height: 100,
-//     width: 100,
-//   },
-//   header: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   text: {
-//     textAlign: 'center',
-//     color: '#333',
-//     marginBottom: 5,
-//   },
-//   buttons: {
-//     justifyContent: 'space-between',
-//     flexDirection: 'row',
-//     margin: 20,
-//     marginBottom: 30,
-//   },
-// });
