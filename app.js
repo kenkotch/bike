ef9f4e234225cc1148e42f2344daec0358129569
SEAN LEMBERG <SEANLEMBERG@GMAIL.COM>
2017-11-19 16:31:32 -0700

    functioning patch

--- app.js
+++ app.js
@@ -11,15 +11,52 @@ import Header from './components/Header'
 import Maintenance from './components/Maintenance'
 import Bikes from './components/Bikes'
 
+let fetchThis = 'https://roads.googleapis.com/v1/snapToRoads?path='
+
 export default class App extends Component {
   constructor(props){
     super(props)
-    this.state={user:undefined, name: '', total_mileage: '', tires: '', chain: '', brake_pads: ''}
+    this.state={
+      user:undefined,
+       name: '',
+       total_mileage: '',
+       tires: '',
+       chain: '',
+       brake_pads: '',
+       test: 'test',
+       locations: [],
+       holder: [],
+       waiter: [],
+       responser: [],
+       distanceAppender: '',
+       appResponse: [],
+       fetchThis: 'https://roads.googleapis.com/v1/snapToRoads?path='
+     };
+     this.getLocation = this.getLocation.bind(this)
   }
 
   // state = {
   //   user: undefined, // user has not logged in yet
   // };
+
+  showPosition = (position) => {
+  this.setState({
+    holder: [this.state.holder] + '|' + [position.coords.latitude, position.coords.longitude]
+  });
+}
+
+getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+      // console.log("success!");
+    } else {
+      console.log('broken')
+    }
+  };
+
+  state = {
+    user: undefined, // user has not logged in yet
+  };
 
   // Set up Linking
   componentDidMount() {
@@ -74,28 +111,101 @@ export default class App extends Component {
 
   render() {
     const { user } = this.state;
-      // fetch('https://my-bike.herokuapp.com/components', {
-      //      headers: {
-      //        'Accept': 'application/json',
-      //        'Content-Type': 'application/json'
-      //      },
-      //      method: 'PATCH',
-      //      body: JSON.stringify( {email: this.state.user, mileage: 20})
-      //    }).then((response) => response.json())
-      //   .then((responseJson) => {
-      //     console.log(responseJson)
-      //   })
+
       fetch('https://my-bike.herokuapp.com/bikes', {
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         method: 'POST',
-        body: JSON.stringify( {email: this.state.user})
+        body: JSON.stringify( {email: 'sean.lemberg@gmail.com'})
       }).then((response) => response.json())
       .then((responseJson) => {
         this.setState({name: responseJson[0]['name'], total_mileage: responseJson[0]['total_mileage'], tires: responseJson[1]['tires'], chain: responseJson[1]['chain'], brake_pads: responseJson[1]['brake_pads']})
       })
+
+          let fetching;
+
+          let parentRender = () => {
+              renderTime();
+              responser();
+            }
+
+            let renderTime = () => {
+              let inceptionArray = this.state.holder.slice(1, this.state.holder.length);
+              fetching = `${fetchThis}` + inceptionArray + `&interpolate=true&key=AIzaSyBQSSqtL6ZXfausABmganfrNw6M6vZlXb0`
+              this.setState({waiter: fetching})
+            }
+
+            let responser = () => {
+              return fetch(`${fetching}`).then((response) => response.json()).then((responseJson) => {
+                this.setState({responser: responseJson})
+                let lat1 = this.state.responser.snappedPoints[0].location.latitude
+                let lon1 = -122.233// this.state.responser.snappedPoints[0].location.longitude
+                let lat2 = this.state.responser.snappedPoints[this.state.responser.snappedPoints.length - 1].location.latitude
+                let lon2 = this.state.responser.snappedPoints[this.state.responser.snappedPoints.length - 1].location.longitude
+                getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2)
+              }).catch((error) => {
+                console.error(error);
+              });
+            }
+            let theMagicHappen = () => {
+              console.log(typeof Math.round(this.state.distanceAppender))
+
+
+            fetch('https://my-bike.herokuapp.com/components', {
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                method: 'PATCH',
+                body: JSON.stringify( {email: 'sean.lemberg@gmail.com', mileage: Math.round(this.state.distanceAppender)})
+              }).then((response) => response.json())
+             .then((responseJson) => {
+               this.state({
+                 appResponse: responseJson
+               })
+               console.log(responseJson)
+             })
+              }
+            // Haversine Formula
+            // console.log("before distance", this.state.distanceAppender)
+      let getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+          var R = 6371; // Radius of the earth in km
+          var dLat = deg2rad(lat2 - lat1); // deg2rad below
+          var dLon = deg2rad(lon2 - lon1);
+          var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+          var d = R * c; // Distance in km
+          var mileCount = ( d * 0.621371) // conversion to Miles
+
+          this.setState({distanceAppender: mileCount})
+          theMagicHappen()
+          //console.log(this.state.distanceAppender)
+          // console.log("TARGET", this.state.distanceAppender);
+      }
+        // console.log("after distance", this.state.distanceAppender)
+
+      //console.log(this.state.distanceAppender)
+
+      let deg2rad = (deg) => deg * (Math.PI / 180)
+
+      let stoppingWaterfall = () => {
+        setTimeout(this.getLocation, 100)
+        parentRender();
+        // theMagicHappen();
+      }
+
+      let timeInitiate = () => {
+        setTimeout(this.getLocation, 100)
+      }
+
+      // console.log('before magic', this.state.distanceAppender)
+
+        // console.log('after magic', this.state.distanceAppender)
+        // move function above the other
+        // see if this alters stateful scope
+
 
 
     return (
@@ -110,6 +220,22 @@ export default class App extends Component {
               <Text>tires { this.state.tires }</Text>
               <Text>chain { this.state.chain }</Text>
               <Text>brakes { this.state.brake_pads }</Text>
+              {/* <Text>{this.state.user}</Text> */}
+              {/* <Text>{this.state.holder}</Text> */}
+              <Text>{this.state.distanceAppender}</Text>
+              {/* <Text>{this.responseJson}</Text> */}
+              <Button block full dark
+              onPress={timeInitiate}
+              style={styles.startButtonStyle}
+              >
+              <Text style={{fontFamily: 'Muli-Light'}}>S T A R T</Text>
+            </Button>
+            <Button block full dark
+              onPress={stoppingWaterfall}
+              style={styles.stopButtonStyle}
+            >
+              <Text style={{fontFamily: 'Muli-Light'}}>S T O P</Text>
+            </Button>
             </View>
           : // Show log in message if not
 
